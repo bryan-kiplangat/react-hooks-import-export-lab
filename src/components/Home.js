960f5eb6050d16283f411f1fb6a3e82af2490a7c
bryan-kiplangat <kipbryan.bett@gmail.com>
2023-10-26 19:16:23 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+//import named export
+import {username, city} from "../data/user"
 
 function Home() {
   return (
@@ -9,3 +11,4 @@ function Home() {
     </div>
   );
 }
+export default Home;
